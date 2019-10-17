0844e3d3f24ed098b37d6acb490de8450f7c2ab3
rmorgan <ramorgan212@gmail.com>
2019-10-17 15:40:43 -0400

    add post view

---
+++ laravel/resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+    <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Post</title>
+</head>
+<body>
+<h1>My Blog post</h1>
+<p>
+    {{$post}}
+</p>
+</body>
+</html>
