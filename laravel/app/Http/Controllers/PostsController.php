9a2f5db4fb975000b524ee6082d1cdf2368980b3
rmorgan <ramorgan212@gmail.com>
2020-03-31 15:58:57 -0400

    small edits to look at functionality.

--- laravel/app/Http/Controllers/PostsController.php
+++ laravel/app/Http/Controllers/PostsController.php
@@ -9,7 +9,8 @@ class PostsController extends Controller
 {
     public function show($slug)
     {
-//        $post = DB::table('posts')->where('slug', $slug)->first();
+//        $post = \DB::table('posts')->where('slug', $slug)->first();
+//        $post = \DB::table('posts')->where('id', $slug)->first();
         $post = Post::where('slug', $slug)->firstOrFail();
         return view('post', [
             'post' => $post,
