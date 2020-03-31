11006528dad36ecaedde166d5a9222e0fce393b4
rmorgan <ramorgan212@gmail.com>
2020-03-31 17:03:12 -0400

    add about page and pull header section out and put into layout tpl

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -57,3 +57,9 @@ Route::get('/posts/{pid}', 'PostsController@show');
 Route::get('/contact', function(){
     return view('contact');
 });
+
+
+Route::get('/about', function(){
+    return view('about');
+});
+
