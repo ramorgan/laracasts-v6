058af303dd5a6785a3f4ce59afdbcf24d53f4088
rmorgan <ramorgan212@gmail.com>
2020-03-31 19:06:15 -0400

    basic methods layouts

--- laravel/app/Http/Controllers/ArticlesController.php
+++ laravel/app/Http/Controllers/ArticlesController.php
@@ -6,11 +6,11 @@ use Illuminate\Http\Request;
 use App\Article;
 
 
-
 class ArticlesController extends Controller
 {
     public function show($id)
     {
+        // show a single resource.
         $article = Article::find($id);
 
         return view('articles.show', ['article' => $article]);
@@ -18,9 +18,37 @@ class ArticlesController extends Controller
 
     public function index()
     {
+        // render a list of a resource.
         $articles = [];
 
         $articles = Article::take(3)->latest()->get();
+
         return view('articles.index', ['articles' => $articles]);
+    }
+
+
+    public function create()
+    {
+        // shows a view to create a resource.
+    }
+
+    public function store()
+    {
+        // Persist the new resource.
+    }
+
+    protected function edit()
+    {
+        // Show a form to edit an existing resource.
+    }
+
+    protected function update()
+    {
+        // Persist the edit resource.
+    }
+
+    protected function destroy()
+    {
+        // Delete the resource.
     }
 }
