d2bc7729de5e0ba510dbe23c6d814df826d419de
rmorgan <ramorgan212@gmail.com>
2020-03-31 16:19:49 -0400

    add complete method to assignment.

--- laravel/app/Assignment.php
+++ laravel/app/Assignment.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Assignment extends Model
 {
-    //
+    public function complete()
+    {
+        $this->completed = true;
+        $this->save();
+    }
 }
