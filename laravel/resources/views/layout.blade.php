ab4ad44ca5d5991f157ea26763dda52d4835ad5c
rmorgan <ramorgan212@gmail.com>
2020-03-31 17:31:08 -0400

    stuff with css and js with laravel mix

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -19,6 +19,7 @@ Released   : 20140225
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
     <link href="css/default.css" rel="stylesheet" type="text/css"/>
     <link href="css/fonts.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="/css/app.css">
 
 </head>
 <body>
@@ -45,5 +46,6 @@ Released   : 20140225
     <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a
                 href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
 </div>
+<script src="/js/app.js"></script>
 </body>
 </html>
