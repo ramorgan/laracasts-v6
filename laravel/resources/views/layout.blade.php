e3480998e1807f99cb033186ebf20cd27da02884
rmorgan <ramorgan212@gmail.com>
2020-05-14 20:53:16 -0400

    added refs that point to nowhere on the menu so that I could get it to work...

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -31,16 +31,22 @@ Released   : 20140225
         </div>
         <div id="menu">
             <ul>
-                <li class="{{ Request::path() === '/' ? 'current_page_item':'' }}"><a href="/" accesskey="1" title="">Homepage</a>
+                <li class="{{ Request::path() === '/' ? 'current_page_item':'' }}">
+                    <a href="/" accesskey="1" title="">Homepage</a>
                 </li>
-                <li class="{{ Request::path() === '#' ? 'current_page_item':'' }}"><a href="#" accesskey="2" title="">Our
-                        Clients</a></li>
-                <li class="{{ Request::path() === '/about' ? 'current_page_item':'' }}"><a href="/about" accesskey="3"
-                                                                                           title="">About Us</a></li>
-                <li class="{{ Request::path() === '/articles' ? 'current_page_item':'' }}"><a href="#" accesskey="4" title="">Articles</a>
+                <li class="{{ Request::path() === '/our_clients' ? 'current_page_item':'' }}">
+                    <a href="/our_clients" accesskey="2" title="">Our Clients</a>
                 </li>
-                <li class="{{ Request::path() === '#' ? 'current_page_item':'' }}"><a href="#" accesskey="5" title="">Contact
-                        Us</a></li>
+                <li class="{{ Request::path() === '/about' ? 'current_page_item':'' }}">
+                    <a href="/about" accesskey="3" title="">About Us</a>
+                </li>
+                <li class="{{ Request::path() === '/articles' ? 'current_page_item':'' }}">
+                    <a href="/articles" accesskey="4" title="">Articles</a>
+                </li>
+                <li class="{{ Request::path() === '/contact' ? 'current_page_item':'' }}">
+                    <a href="/contact" accesskey="5" title="">Contact Us</a>
+                </li>
+                <li><a href="https://www.google.com">Google</a></li>
             </ul>
         </div>
     </div>
