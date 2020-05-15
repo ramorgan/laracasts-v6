8f27535b18ef95b792a26b052219e15c9051a956
rmorgan <ramorgan212@gmail.com>
2020-05-15 00:54:04 -0400

    Add Tag. create many to many relationship between Articles and Tags.

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -18,4 +18,8 @@ class Article extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
